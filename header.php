499236dd0e04ea770b9821916a50d5fc5a72502c
najmi10 <53500211+najmi10@users.noreply.github.com>
2020-03-28 08:23:44 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<link rel="stylesheet" href="http://localhost/siperpus/aset/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost/siperpus/aset/fontawesome/css/all.min.css">
+
+<title>Nature Library</title>
+</head>
+<body>
+<script src="http://localhost/siperpus/aset/jquery.js"></script>
+   <script src="http://localhost/siperpus/aset/bootstrap/js/bootstrap.min.js"></script>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>Nature|Perpustakaan</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="navbar-nav">
+<a class="nav-item nav-link active" href="http://localhost/siperpus/index.php">Dashboard</a>
+<a class="nav-item nav-link" href="http://localhost/siperpus/buku/index.php">Buku</a>
+<a class="nav-item nav-link" href="http://localhost/siperpus/anggota/index.php">Anggota</a>
+<a class="nav-item nav-link" href="http://localhost/siperpus/transaksi/index.php">Peminjaman</a>
+<a class="nav-item nav-link" href="#">Logout</a>
+</div>
+</div>
+</nav>
+</body>
+</html>
+
